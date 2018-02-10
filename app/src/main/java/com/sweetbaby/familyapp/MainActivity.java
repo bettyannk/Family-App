5e91dacf0b08476c9eece85e50f1b0c56344457b
simiyu1 <mbchb1990@gmail.com>
2018-02-10 09:37:12 +0300

    Added the navigation View menu items
    placed a tabbed activity on the MapView

--- app/src/main/java/com/sweetbaby/familyapp/MainActivity.java
+++ app/src/main/java/com/sweetbaby/familyapp/MainActivity.java
@@ -1,15 +1,28 @@
 package com.sweetbaby.familyapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.sweetbaby.familyapp.History.HistoryMain;
+
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback{
+
+    Button viewfam;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,14 +31,34 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        viewfam= findViewById(R.id.viewFam);
+
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+
+        viewfam.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(MainActivity.this, MapperActivity.class);
+                //startActivity(intent);
+
+            }
+        });
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                /*Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();*/
+                Intent intent = new Intent(MainActivity.this, HistoryMain.class);
+                startActivity(intent);
             }
         });
+
+
     }
 
     @Override
@@ -48,5 +81,39 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        googleMap.addMarker(new MarkerOptions().position(new LatLng(0, 0)).title("Marker"));
+    }
+
+    //@SuppressWarnings("StatementWithEmptyBody")
+    //@Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_view) {
+            // Handle
+            Intent intent = new Intent(MainActivity.this, HistoryMain.class);
+            startActivity(intent);
+            finish();
+        } else if (id == R.id.nav_messages) {
+
+
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_family) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
